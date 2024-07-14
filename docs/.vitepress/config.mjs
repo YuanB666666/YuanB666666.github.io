e0920819646aa78afb6abe61d2efd198d42f5dba
YuanB666666 <liudrr@proton.me>
2024-07-15 01:26:29 +0800

    优化网站显示内容

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,13 +2,37 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
+  title: "混元宝 | 只想让你混得更好",
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    // 底部配置
+    footer: {
+      copyright: "copyright@ 2024 HunYuanBao",
+    },
+    // 搜索框设置
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索资源",
+            buttonAriaLabel: "搜索资源",
+          },
+          modal: {
+            noResultsText: "没有找到相关资源",
+            resetButtonTitle: "清空搜索关键词",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '主页', link: '/' },
+      { text: '菜单', link: '/markdown-examples' }
     ],
 
     sidebar: [
--- docs/index.md
+++ docs/index.md
@@ -4,19 +4,23 @@ layout: home
 
 hero:
   name: "混元宝"
-  text: "A VitePress Site"
-  tagline: My great project tagline
+  text: "要想混得好就来混元宝"
+  tagline: 你想要的宝贝这里全都有
+  image:
+    src: /logo.png
+    alt: 背景图
   actions:
     - theme: brand
-      text: Markdown Examples
+      text: 开始探索
       link: /markdown-examples
     - theme: alt
-      text: API Examples
+      text: 加入电报
       link: /api-examples
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - icon: 💪
+    title: 行行有货源
+    details: 古话说 360 行，行行出状元。</br>而混元宝的宗旨则是希望做到行行有货源！</br>尽自己所能有效解决大家的燃眉之急！
   - title: Feature B
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
   - title: Feature C
